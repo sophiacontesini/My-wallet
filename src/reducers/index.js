48f38c75c6d7356a67551bb48c6be21368c8c32a
sophiacontesini <sophiamarques214@gmail.com>
2022-03-31 19:14:16 -0300

    add req 2

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,11 @@ import { combineReducers } from 'redux';
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
 import user from './user';
-import walletReducer from './wallet';
+import wallet from './wallet';
 
 const rootReducer = combineReducers({
   user,
-  walletReducer,
+  wallet,
 });
 
 export default rootReducer;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -5,11 +5,11 @@ const INITIAL_STATE = {
   expenses: [],
 };
 
-const walletReducer = (state = INITIAL_STATE, action) => {
+const wallet = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   default:
     return state;
   }
 };
 
-export default walletReducer;
+export default wallet;
